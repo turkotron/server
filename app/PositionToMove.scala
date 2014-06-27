6bd079ad93a732afcc16e1202296860f9e7c0035
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-06-27 11:51:22 +0200

    implement naive position to move

--- app/PositionToMove.scala
+++ app/PositionToMove.scala
@@ -12,13 +12,15 @@ object PositionToMove {
     val found = for {
       missing <- findMissing(pos1, pos2)
       added <- findAdded(pos1, pos2)
-    } yield "e2e4"
+    } yield s"$missing$added"
     found getOrElse sys.error(s"Can't find move\n$pos1\n$pos2")
   }
 
   private def findMissing(pos1: Pos, pos2: Pos): Option[Square] =
-    None
+    (pos1.keySet diff pos2.keySet).headOption
 
   private def findAdded(pos1: Pos, pos2: Pos): Option[Square] =
-    None
+    pos2 find {
+      case (square, color) => (pos1 get square) != Some(color)
+    } map (_._1)
 }
